8abf81c42f5c82a720d54266d9444d8e34118a50
Nevacat <nevacattery217@naver.com>
2024-03-14 20:24:55 +0900

    conf:프로젝트 추가 셋팅

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+const config = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
