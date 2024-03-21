7c98525ff95cc1c97193977dd3cc5dbca06e756a
aliyah521 <aliyah521@bitgarma.biz>
2024-03-21 11:09:23 +0900

    Style : 버튼 공통 컴포넌트 customtype 추가

--- src/components/@common/Button.tsx
+++ src/components/@common/Button.tsx
@@ -5,11 +5,28 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   name?: string
   children?: React.ReactNode
   addClass?: string
+  customType?: string
 }
 
-const Button = ({ name, children, addClass, ...props }: ButtonProps) => {
+export enum type {
+  fill = 'fill',
+  empty = 'empty',
+}
+
+const Button = ({ name, children, addClass, customType, ...props }: ButtonProps) => {
   return (
-    <button className={cn('py-2 px-5 rounded-lg text-white bg-main-medium', addClass)} {...props}>
+    <button
+      className={cn(
+        'py-2 px-5 rounded-lg',
+        customType === type.fill
+          ? ' text-white bg-main-medium'
+          : customType === type.empty
+            ? 'text-default bg-transparent border border-default border-opacity-20 '
+            : '',
+        addClass,
+      )}
+      {...props}
+    >
       {name && name}
       {children && children}
     </button>
