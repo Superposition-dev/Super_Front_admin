861b708ff8696eebdc70369f1985e9aa40856d17
Nevacat <nevacattery217@naver.com>
2024-04-01 12:29:44 +0900

    style:레이아웃 구조 변경

--- src/components/@common/Layout.tsx
+++ src/components/@common/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = (props: LayoutProps) => {
   const { children } = props
   return (
     <div className="flex justify-center w-full h-screen max-w-[2520px]">
-      <div className="relative flex w-full h-screen">
+      <div className="relative flex flex-col sm:flex-row  w-full h-screen">
         <Sidebar />
         <div className="w-full h-full bg-[#fafafa]">{children}</div>
       </div>
