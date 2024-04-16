69fae0c5826b5e2ff88f4db1bef717451dbfa0de
aliyah521 <aliyah521@bitgarma.biz>
2024-04-16 15:10:00 +0900

    Feat : 회원 구분 컴포넌트 로직 변경

--- src/components/@common/UserType.tsx
+++ src/components/@common/UserType.tsx
@@ -1,31 +1,34 @@
 import cn from '../../lib/tailwindUtil'
 
 export interface UserTypeProps {
-  isAuthor: boolean
+  general: boolean
+  list: Array<string>
+  onGeneral?: () => void
+  onNotGeneral?: () => void
   addClass?: string
 }
 
-const UserType = ({ isAuthor, addClass }: UserTypeProps) => {
+const UserType = ({ general, list, onGeneral, onNotGeneral, addClass }: UserTypeProps) => {
   return (
     <div
       className={cn(
-        'w-[360px] flex justify-between items-center min-h-[64px] h-fit py-3 px-4 rounded-2xl bg-white shadow-light',
+        'w-[360px] flex justify-between items-center min-h-[64px] h-fit py-3 px-4 rounded-2xl bg-white shadow-light border border-transparent',
         addClass,
       )}
     >
       <p className="text-xl font-semibold">회원 구분</p>
       <div className=" flex gap-4 rounded-md">
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 cursor-default" onClick={onGeneral}>
           <div className="w-6 h-6 border border-gray-300 rounded-full flex justify-center items-center">
-            {!isAuthor && <div className="w-4 h-4 bg-main-medium rounded-full"></div>}
+            {general && <div className="w-4 h-4 bg-main-medium rounded-full"></div>}
           </div>
-          <p>일반</p>
+          <p>{list[0]}</p>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 cursor-default" onClick={onNotGeneral}>
           <div className="w-6 h-6 border border-gray-300 rounded-full flex justify-center items-center">
-            {isAuthor && <div className="w-4 h-4 bg-main-medium rounded-full"></div>}
+            {!general && <div className="w-4 h-4 bg-main-medium rounded-full"></div>}
           </div>
-          <p>일반 + 작가</p>
+          <p>{list[1]}</p>
         </div>
       </div>
     </div>
