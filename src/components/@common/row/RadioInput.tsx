075e524c25c3aeb41e3ce0327475d00ea5e16914
Nevacat <nevacattery217@naver.com>
2024-04-17 13:34:22 +0900

    feat:Radio Input 컴포넌트 작성

---
+++ src/components/@common/row/RadioInput.tsx
@@ -0,0 +1,54 @@
+import cn from '../../../lib/tailwindUtil'
+import Button, { type } from '../atom/Button'
+
+export interface ButtonType {
+  title: string
+  onClick: () => void
+}
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  title: string
+  values: {
+    value: string,
+    text: string
+  }[]
+  constant?: boolean
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  disabled?: boolean
+}
+
+const RadioInput = ({ title, values,constant, onChange, disabled,...props}: InputProps) => {
+  return (
+    <div className="flex flex-col gap-2.5 w-full h-full">
+      <h4 className="font-semibold">
+        {title}
+        {props.required && <span className="text-main-bright">*</span>}
+      </h4>
+      <div
+        className={cn(
+          'h-[80%] flex items-center w-full border-main-too-dark overflow-hidden',
+          !disabled ? '' : '',
+        )}
+      >
+        {
+          values.map((data) => (
+            <label className='flex w-full '>
+              <input
+                className={cn('w-[30%]', !constant && 'disabled:bg-transparent')}
+                type='radio'
+                name={title}
+                value={data.value}
+                checked={props.value === data.value}
+                onChange={onChange}
+                disabled={disabled}
+              />
+              <span>{data.text}</span>
+            </label>
+          ))
+        }
+      </div>
+    </div>
+  )
+}
+
+export default RadioInput
